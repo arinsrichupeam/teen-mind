3b158de96394d99bc9f7867047c14ddb9b7460e8
arinsrichupeam <arinsrichupeam@gmail.com>
2025-02-03 13:41:50 +0700

    Update Home

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,16 @@ export default function IndexPage() {
     }
   };
 
+  useEffect(() => {
+
+  }, [session]);
+
   return (
     <div className="flex flex-col gap-5 items-center">
       <h1 className={title()}>TEEN MIND</h1>
       <Card className="mt-4 pt-4 pb-4 px-4">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <small className="text-default-500">12 Tracks</small>
+          <small className="text-default-500">{status}</small>
           <h4 className="font-bold text-large">Frontend Radio</h4>
         </CardHeader>
         <CardBody className="overflow-visible py-2">
@@ -51,9 +55,10 @@ export default function IndexPage() {
         </CardBody>
         <CardFooter className="flex-col gap-4 items-start">
           <p className="text-tiny uppercase font-bold">คำอธิบายเกี่ยวกับแอพ</p>
+          <Button className="w-full" variant="solid" color="primary" size="lg" radius="full" onPress={() => signOut()}>ออกจากระบบ</Button>
           <Button className="w-full" variant="solid" color="primary" size="lg" radius="full" onPress={CheckProfile}>ถัดไป</Button>
         </CardFooter>
       </Card>
-    </div>
+    </div >
   );
 }
