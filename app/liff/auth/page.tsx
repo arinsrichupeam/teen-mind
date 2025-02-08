f0174de906e8c03d595ec904ec71a9cdf0eb215b
Arin Srichupeam <arinsrichupeam@gmail.com>
2025-02-08 11:09:50 +0700

    Update Auth Page

--- app/liff/auth/page.tsx
+++ app/liff/auth/page.tsx
@@ -13,16 +13,18 @@ export default function AuthPage() {
   return (
     <section className="flex flex-col w-[calc(100vw)] h-[calc(100vh-48px)] items-center gap-4 pt-10 px-8 py-8 md:py-10">
       <Suspense fallback={<Loading />}>
-        <Image src="/image/LogoBKK.png" />
-        <div className="text-center justify-center pt-[calc(25vh)]">
+        <Image alt="BKK Logo" loading="lazy" src="/image/LogoBKK.png" />
+
+        <div className=" flex flex-col text-center justify-center pt-[calc(10vh)] gap-20">
+          <Image alt="BKK Logo" loading="lazy" src="/image/logo_App.png" />
           <p className={title({ size: "sm" })}>เข้าสู่ระบบ</p>
         </div>
 
         <div className="flex gap-3">
           <Button
             isIconOnly
-            aria-label="Like"
-            className="w-16 h-16"
+            aria-label="Line"
+            className="w-14 h-14"
             color="warning"
             variant="faded"
             onPress={() => signIn("line")}
