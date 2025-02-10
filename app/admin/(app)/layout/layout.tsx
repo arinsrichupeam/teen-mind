1223304004307628eebc01863691c6c71ff706fd
Arin Srichupeam <arinsrichupeam@gmail.com>
2025-02-10 16:05:49 +0700

    Update Admin Page

---
+++ app/admin/(app)/layout/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+import { SidebarWrapper } from "../sidebar/sidebar";
+
+import { SidebarContext } from "@/app/admin/(app)/layout/layout-context";
+import { NavbarWrapper } from "@/app/admin/(app)/components/navbar_admin";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const handleToggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        collapsed: sidebarOpen,
+        setCollapsed: handleToggleSidebar,
+      }}
+    >
+      <section className="flex">
+        <SidebarWrapper />
+        <NavbarWrapper>{children}</NavbarWrapper>
+      </section>
+    </SidebarContext.Provider>
+  );
+}
