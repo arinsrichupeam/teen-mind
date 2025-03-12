1f028ffb9e53c43e4002ebcd188caee8cc00f4ee
This.X <rockerzeeza@gmail.com>
2025-03-12 16:58:10 +0700

    Add referent data retrieval and enhance registration UI

---
+++ app/api/data/referent/[id]/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/utils/prisma";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+  const data = await prisma.referent.findMany({
+    where: {
+      id: parseInt(id),
+    },
+    select: {
+      firstname: true,
+      lastname: true,
+    },
+  });
+
+  return Response.json(data);
+}
+
