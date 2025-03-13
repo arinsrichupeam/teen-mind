2f180ffbf891e913d66301cdebc283ab2f2f34ae
This.X <rockerzeeza@gmail.com>
2025-03-13 14:56:39 +0700

    No code changes made.

--- app/liff/question/phqa/page.tsx
+++ app/liff/question/phqa/page.tsx
@@ -14,6 +14,7 @@ import {
   ModalBody,
   ModalContent,
   ModalFooter,
+  ModalHeader,
   useDisclosure,
 } from "@heroui/modal";
 import { Divider, InputOtp } from "@heroui/react";
@@ -24,65 +25,56 @@ import Loading from "@/app/loading";
 import { qPhqa_addon, qPhqa } from "@/app/data";
 
 export default function PHQAPage() {
-  const qPhqa_Image = (key: number) => [
+  const qPhqa_Image = [
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-01.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-02.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-03.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-04.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-05.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-06.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-07.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q1-08.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
@@ -90,16 +82,14 @@ export default function PHQAPage() {
     />,
   ];
 
-  const qPhqa_addon_Image = (key: number) => [
+  const qPhqa_addon_Image = [
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
       src="/image/Q2-01.png"
     />,
     <Image
-      key={key}
       alt="PHQA Image"
       className="h-[20vh]"
       loading="lazy"
@@ -132,7 +122,7 @@ export default function PHQAPage() {
   const router = useRouter();
   const { data: session, status } = useSession();
   const [questionName, setQuestionName] = useState("PHQ-A");
-  const [progress, setProgress] = useState(9);
+  const [progress, setProgress] = useState(0);
   const [showPHQA, setPHQAShow] = useState(true);
   const [submit, setSubmit] = useState(true);
   const [userId, setUserId] = useState("");
@@ -306,10 +296,10 @@ export default function PHQAPage() {
           <ModalContent>
             {() => (
               <>
-                <ModalBody className="items-center text-center pt-10">
-                  <h2 className={title({ size: "xs" })}>
-                    กรอกรหัสผู้ให้คำแนะนำ
-                  </h2>
+                <ModalHeader className="flex flex-col items-center font-semibold text-2xl">
+                  กรอกรหัสผู้ให้คำแนะนำ
+                </ModalHeader>
+                <ModalBody className="items-center text-center">
                   <Divider />
                   <div className="flex justify-center">
                     <InputOtp
@@ -368,9 +358,11 @@ export default function PHQAPage() {
             )}
           </ModalContent>
         </Modal>
+
         <h1 className={title({ size: "xs" })}>
           แบบประเมินภาวะซึมเศร้าในวัยรุ่น
         </h1>
+
         <Progress
           aria-label="Loading..."
           className="max-w-md"
@@ -390,96 +382,102 @@ export default function PHQAPage() {
           >
             {showPHQA
               ? qPhqa.map((val, index) => {
-                  return (
-                    <Tab key={index}>
-                      <div className="flex flex-col gap-4 mt-[-50px]">
-                        <div className="flex flex-col items-center">
-                          {qPhqa_Image(index)[index]}
-                        </div>
-                        <div className="flex flex-col gap-4 items-start text-start">
-                          <p className="text-primary-500 font-semibold">
-                            {index + 1}. {val}
-                          </p>
-                          <div className="flex flex-col gap-2 w-full mt-[-15px] ml-[-5px]">
-                            <RadioGroup
-                              key={index}
-                              className="pl-5"
-                              label="ในช่วง 2 สัปดาห์ คุณมีอาการเหล่านี้บ่อยแค่ไหน"
-                              name={(index + 1).toString()}
-                              onChange={(val) => phqaChange(val)}
+                return (
+                  <Tab key={index}>
+                    <div className="flex flex-col gap-4 mt-[-50px]">
+                      <div className="flex flex-col items-center">
+                        {qPhqa_Image[index]}
+                      </div>
+                      <div className="flex flex-col gap-4 items-start text-start">
+                        <p className="text-primary-500 font-semibold">
+                          {index + 1}. {val}
+                        </p>
+                        <div className="flex flex-col gap-2 w-full mt-[-15px] ml-[-5px]">
+                          <RadioGroup
+                            key={index}
+                            className="pl-5"
+                            label="ในช่วง 2 สัปดาห์ คุณมีอาการเหล่านี้บ่อยแค่ไหน"
+                            name={(index + 1).toString()}
+                            onChange={(val) => {
+                              phqaChange(val);
+                              setProgress(calProgress(index + 1));
+                            }}
+                          >
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="0"
                             >
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="0"
-                              >
-                                ไม่มีเลย
-                              </Radio>
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="1"
-                              >
-                                มีบางวัน
-                              </Radio>
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="2"
-                              >
-                                มีมากกว่า 7 วัน
-                              </Radio>
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="3"
-                              >
-                                มีแทบทุกวัน
-                              </Radio>
-                            </RadioGroup>
-                          </div>
+                              ไม่มีเลย
+                            </Radio>
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="1"
+                            >
+                              มีบางวัน
+                            </Radio>
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="2"
+                            >
+                              มีมากกว่า 7 วัน
+                            </Radio>
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="3"
+                            >
+                              มีแทบทุกวัน
+                            </Radio>
+                          </RadioGroup>
                         </div>
                       </div>
-                    </Tab>
-                  );
-                })
+                    </div>
+                  </Tab>
+                );
+              })
               : qPhqa_addon.map((val, index) => {
-                  return (
-                    <Tab key={index + 9}>
-                      <div className="flex flex-col  gap-4 mt-[-50px]">
-                        <div className="flex flex-col items-center">
-                          {qPhqa_addon_Image(index)[index]}
-                        </div>
-                        <div className="flex flex-col gap-4 items-start text-start">
-                          <p className="text-primary-500 font-semibold">
-                            {index + 1}. {val}
-                          </p>
-                          <div className="flex flex-col gap-2 w-full mt-[-15px] ml-[-5px]">
-                            <RadioGroup
-                              key={index}
-                              className="pl-5"
-                              label="เลือกข้อที่รู้สึกตรงกับตัวเอง"
-                              name={"1" + index.toString()}
-                              value={Object.entries(val)
-                                [index + 2].toString()
-                                .substring(3)}
-                              onChange={(val) => phqaAddonChange(val)}
+                return (
+                  <Tab key={index + 9}>
+                    <div className="flex flex-col  gap-4 mt-[-50px]">
+                      <div className="flex flex-col items-center">
+                        {qPhqa_addon_Image[index]}
+                      </div>
+                      <div className="flex flex-col gap-4 items-start text-start">
+                        <p className="text-primary-500 font-semibold">
+                          {index + 1}. {val}
+                        </p>
+                        <div className="flex flex-col gap-2 w-full mt-[-15px] ml-[-5px]">
+                          <RadioGroup
+                            key={index}
+                            className="pl-5"
+                            label="เลือกข้อที่รู้สึกตรงกับตัวเอง"
+                            name={"1" + index.toString()}
+                            value={Object.entries(val)
+                            [index + 2].toString()
+                              .substring(3)}
+                            onChange={(val) => {
+                              phqaAddonChange(val);
+                              setProgress(calProgress(index + 10));
+                            }}
+                          >
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="0"
                             >
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="0"
-                              >
-                                ไม่ใช่
-                              </Radio>
-                              <Radio
-                                className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
-                                value="1"
-                              >
-                                ใช่
-                              </Radio>
-                            </RadioGroup>
-                          </div>
+                              ไม่ใช่
+                            </Radio>
+                            <Radio
+                              className="inline-flex m-0  items-center justify-between flex-row-reverse max-w-full cursor-pointer rounded-xl p-3 border"
+                              value="1"
+                            >
+                              ใช่
+                            </Radio>
+                          </RadioGroup>
                         </div>
                       </div>
-                    </Tab>
-                  );
-                })}
+                    </div>
+                  </Tab>
+                );
+              })}
           </Tabs>
         </div>
 
