1d482e487fbe541561e7ca440724fa3ee7246cec
This.X <rockerzeeza@gmail.com>
2025-07-31 14:36:45 +0700

    เพิ่มฟังก์ชันใหม่ใน route.ts สำหรับการจัดการข้อความ Flex ใหม่ ได้แก่ GreenLowFlex และ OrangeFlex รวมถึงการปรับปรุง URL ของภาพใน GreenFlex, YellowFlex และ RedFlex เพื่อให้มีความเหมาะสมและชัดเจนยิ่งขึ้น

--- config/site.ts
+++ config/site.ts
@@ -81,7 +81,7 @@ export const GreenFlex: FlexMessage = {
         type: "bubble",
         hero: {
           type: "image",
-          url: "https://img2.pic.in.th/pic/7ffa7302b2c19f7f759fadad457223b9.jpg",
+          url: "https://img2.pic.in.th/pic/green.jpeg",
           size: "full",
           aspectRatio: "20:13",
           aspectMode: "cover",
@@ -250,7 +250,7 @@ export const YellowFlex: FlexMessage = {
         type: "bubble",
         hero: {
           type: "image",
-          url: "https://img5.pic.in.th/file/secure-sv1/-4f4b706e6ae0738b8.jpg",
+          url: "https://img5.pic.in.th/file/secure-sv1/red9fc363b01b1e6818.jpeg",
           size: "full",
           aspectRatio: "20:13",
           aspectMode: "cover",
@@ -417,7 +417,7 @@ export const RedFlex: FlexMessage = {
         type: "bubble",
         hero: {
           type: "image",
-          url: "https://img2.pic.in.th/pic/-54acbf84ef21a74d4.jpg",
+          url: "https://img5.pic.in.th/file/secure-sv1/yellowcb878d777cff5017.jpeg",
           size: "full",
           aspectRatio: "20:13",
           aspectMode: "cover",
@@ -442,6 +442,342 @@ export const RedFlex: FlexMessage = {
                 {
                   type: "text",
                   text: "ขณะนี้พบว่าท่านมีความเสี่ยงภาวะซึมเศร้าในระดับปานกลาง/รุนแรง และมีความเสี่ยงฆ่าตัวตาย รบกวนเวลาของท่านให้ข้อมูลส่วนตัวเพิ่มเติมเพื่อส่งต่อให้นักจิตวิทยาได้ติดต่อกลับค่ะ",
+                  weight: "regular",
+                  size: "md",
+                  flex: 0,
+                  wrap: true,
+                },
+              ],
+            },
+            {
+              type: "separator",
+              margin: "xxl",
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "หากต้องการข้อมูลเพิ่มเติมสามารถเข้ารับบริการได้ที่ สายด่วนสุขภาพจิต 1323",
+              weight: "bold",
+              wrap: true,
+            },
+          ],
+        },
+      },
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img2.pic.in.th/pic/-380c9732d402da5f0.jpg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "แอพหมอ. กทม.",
+              weight: "bold",
+              size: "xl",
+              wrap: true,
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "Android",
+                uri: "https://play.google.com/store/apps/details?id=com.msd.bma&hl=th&pli=1",
+              },
+              style: "primary",
+            },
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "IOS",
+                uri: "https://apps.apple.com/th/app/%E0%B8%AB%E0%B8%A1%E0%B8%AD-%E0%B8%81%E0%B8%97%E0%B8%A1/id1600388607",
+              },
+              style: "primary",
+            },
+          ],
+        },
+      },
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img5.pic.in.th/file/secure-sv1/-70a854123a6de2944.jpg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "Application OOCA",
+              weight: "bold",
+              size: "xl",
+              wrap: true,
+            },
+            {
+              type: "text",
+              text: "ดาวน์โหลดฟรี",
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "Android",
+                uri: "https://play.google.com/store/apps/details?id=co.ooca.user&hl=th",
+              },
+              style: "primary",
+            },
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "IOS",
+                uri: "https://apps.apple.com/th/app/ooca-%E0%B8%9B%E0%B8%A3-%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%9B-%E0%B8%8D%E0%B8%AB%E0%B8%B2%E0%B9%83%E0%B8%88/id1260476046",
+              },
+              style: "primary",
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+export const GreenLowFlex: FlexMessage = {
+  type: "flex",
+  altText: "ผลประเมินภาวะซึมเศร้า",
+  contents: {
+    type: "carousel",
+    contents: [
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img2.pic.in.th/pic/green.jpeg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "พบความเสี่ยงเล็กน้อย",
+              weight: "bold",
+              size: "xl",
+              wrap: true,
+            },
+            {
+              type: "box",
+              layout: "baseline",
+              flex: 1,
+              contents: [
+                {
+                  type: "text",
+                  text: "พบความเสี่ยงภาวะซึมเศร้าในระดับเล็กน้อย ขอให้ท่านหมั่นดูแลตนเอง ทั้งร่างกายและจิตใจให้สดชื่นแข็งแรง และหากมีอาการเพิ่มเติมสามารถติดต่อสายด่วนสุขภาพจิตได้",
+                  weight: "regular",
+                  size: "md",
+                  flex: 0,
+                  wrap: true,
+                },
+              ],
+            },
+            {
+              type: "separator",
+              margin: "xxl",
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "หากต้องการข้อมูลเพิ่มเติมสามารถเข้ารับบริการได้ที่ สายด่วนสุขภาพจิต 1323",
+              weight: "bold",
+              align: "start",
+              wrap: true,
+            },
+          ],
+        },
+      },
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img2.pic.in.th/pic/-380c9732d402da5f0.jpg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "แอพหมอ. กทม.",
+              weight: "bold",
+              size: "xl",
+              wrap: true,
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "Android",
+                uri: "https://play.google.com/store/apps/details?id=com.msd.bma&hl=th&pli=1",
+              },
+              style: "primary",
+            },
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "IOS",
+                uri: "https://apps.apple.com/th/app/%E0%B8%AB%E0%B8%A1%E0%B8%AD-%E0%B8%81%E0%B8%97%E0%B8%A1/id1600388607",
+              },
+              style: "primary",
+            },
+          ],
+        },
+      },
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img5.pic.in.th/file/secure-sv1/-70a854123a6de2944.jpg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "Application OOCA",
+              weight: "bold",
+              size: "xl",
+              wrap: true,
+            },
+            {
+              type: "text",
+              text: "ดาวน์โหลดฟรี",
+            },
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "Android",
+                uri: "https://play.google.com/store/apps/details?id=co.ooca.user&hl=th",
+              },
+              style: "primary",
+            },
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "IOS",
+                uri: "https://apps.apple.com/th/app/ooca-%E0%B8%9B%E0%B8%A3-%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%9B-%E0%B8%8D%E0%B8%AB%E0%B8%B2%E0%B9%83%E0%B8%88/id1260476046",
+              },
+              style: "primary",
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+
+export const OrangeFlex: FlexMessage = {
+  type: "flex",
+  altText: "ผลประเมินภาวะซึมเศร้า",
+  contents: {
+    type: "carousel",
+    contents: [
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://img5.pic.in.th/file/secure-sv1/orange.jpeg",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "text",
+              text: "พบความเสี่ยงมาก",
+              weight: "bold",
+              size: "lg",
+              wrap: true,
+            },
+            {
+              type: "box",
+              layout: "baseline",
+              contents: [
+                {
+                  type: "text",
+                  text: "พบความเสี่ยงภาวะซึมเศร้าในระดับมาก ขอให้ท่านติดต่อนักจิตวิทยาหรือจิตแพทย์เพื่อรับการประเมินและรักษาที่เหมาะสม",
                   weight: "regular",
                   size: "md",
                   flex: 0,
